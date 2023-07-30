98c82691b6a916b7246962f9177a8a88aa84d0d6
Max Rustle <csmoke.mk@gmail.com>
2023-07-30 18:12:06 +0100

    exercise 1-7 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,11 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const total = good + neutral + bad
+    const averSum = good - bad
+    const average = averSum / total
+    const positive = good / total * 100
+
     const like = () => setGood(good + 1)
     const idgaf = () => setNeutral(neutral + 1)
     const dislike = () => setBad(bad + 1)
@@ -19,10 +24,14 @@ const App = () => {
             <button onClick={dislike}>Bad!</button>
 
             <h2>Statistics</h2>
-            
+
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
+            <p>Total: {total}</p>
+            <p>Average: {total === 0 ? 0 : average}</p>
+            <p>Positive: {total === 0 ? 0 : positive}%</p>
+
         </>
     )
 }
