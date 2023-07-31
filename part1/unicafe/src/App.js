f301cdb17c1de61887a47d024561d2e7a9815ceb
Max Rustle <csmoke.mk@gmail.com>
2023-07-31 11:48:17 +0100

    exercise 1-8 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,31 @@
 import { useState } from "react"
 
+const Statistics = ({good, neutral, bad}) => {
+    
+    const total = good + neutral + bad
+    const averSum = good - bad
+    const average = averSum / total
+    const positive = good / total * 100
+
+    return (
+        <>
+            <h2>Statistics</h2>
+
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>Total: {total}</p>
+            <p>Average: {total === 0 ? 0 : average}</p>
+            <p>Positive: {total === 0 ? 0 : positive}%</p>
+        </>
+    )
+}
+
 const App = () => {
 
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
-    const total = good + neutral + bad
-    const averSum = good - bad
-    const average = averSum / total
-    const positive = good / total * 100
 
     const like = () => setGood(good + 1)
     const idgaf = () => setNeutral(neutral + 1)
@@ -23,15 +39,7 @@ const App = () => {
             <button onClick={idgaf}>Neutral</button>
             <button onClick={dislike}>Bad!</button>
 
-            <h2>Statistics</h2>
-
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
-            <p>Total: {total}</p>
-            <p>Average: {total === 0 ? 0 : average}</p>
-            <p>Positive: {total === 0 ? 0 : positive}%</p>
-
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </>
     )
 }
